0a71bb7783fd82110fc30332738638e167db9317
German Yanovich <germanyanovich@gmail.com>
2020-08-07 00:50:34 +0300

    change map to FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Navbar from './src/Navbar';
 import AddTodo from './src/AddTodo';
 import Todo from './src/Todo';
@@ -22,11 +22,11 @@ export default function App() {
       <Navbar title='Todo App' />
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
-        <View>
-          {todos.map(todo => (
-            <Todo todo={todo} key={todo.id} />
-          ))}
-        </View>
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} />}
+        />
       </View>
     </View>
   );
