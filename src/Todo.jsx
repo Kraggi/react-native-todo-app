c881cfdf4ba915dfd1f3a74dc1d32696fc73eb1c
German Yanovich <germanyanovich@gmail.com>
2020-08-07 01:17:45 +0300

    added todo deleteHandler on long press

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, onRemove }) => {
   return (
-    <View style={styles.todo}>
-      <Text>{todo.title}</Text>
-    </View>
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={() => console.log('pressed', todo.id)}
+      onLongPress={() => onRemove(todo.id)}
+    >
+      <View style={styles.todo}>
+        <Text>{todo.title}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
