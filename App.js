f129cf5b33bb92073dc1d20295cb72433bceb241
German Yanovich <germanyanovich@gmail.com>
2020-08-07 00:32:08 +0300

    added Todo comp and map it on App

--- App.js
+++ App.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navbar from './src/Navbar';
 import AddTodo from './src/AddTodo';
+import Todo from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = React.useState([]);
+
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title,
+      },
+    ]);
+  };
+
   return (
     <View>
       <Navbar title='Todo App' />
       <View style={styles.container}>
-        <AddTodo />
+        <AddTodo onSubmit={addTodo} />
+        <View>
+          {todos.map(todo => (
+            <Todo todo={todo} key={todo.id} />
+          ))}
+        </View>
       </View>
     </View>
   );
--- src/AddTodo.jsx
+++ src/AddTodo.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
-const AddTodo = () => {
+const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = React.useState('');
+
+  const pressHandler = () => {
+    if (value.trim()) {
+      onSubmit(value);
+      setValue('');
+    } else {
+      Alert.alert('Название дела не может быть пустым');
+    }
+  };
+
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input} />
-      <Button title='Добавить' />
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue}
+        value={value}
+        placeholder='Введите название дела...'
+        autoCorrect={false}
+        autoCapitalize='none'
+      />
+      <Button title='Добавить' onPress={pressHandler} />
     </View>
   );
 };
@@ -17,6 +35,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: 15,
   },
   input: {
     width: '70%',
